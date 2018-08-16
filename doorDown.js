4c91de0d1c2ba38e2cb4937359f95ac46d620487
mljrico <mljrico@yahoo.com>
2018-08-16 18:01:51 -0400

    Initial commit

---
+++ doorDown.js
@@ -0,0 +1,23 @@
+// create a status LED for the amount of time the door needs to go down
+
+let Gpio = require('onoff').Gpio;
+let LED = new Gpio(4, 'out');
+let doorTravel = setInterval(blinkLED, 250);
+
+function blinkLED() {
+    if (LED.readySync() === 0) {
+        LED.writeSync(1);
+    } else {
+        LED.writeSync(0);
+    }
+
+}
+
+function endBlink() {
+    clearInterval(doorTravel);
+LED.writeSync(0);
+LED.unexport();
+}
+
+setTimeout(endBlink, 5000);
+
